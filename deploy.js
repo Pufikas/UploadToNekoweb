590eec3611748cb45c317592e9d81895c691f674
Pufikas <pufikas@proton.me>
2026-01-09 23:34:11 +0200

    upload test

--- deploy.js
+++ deploy.js
@@ -1,13 +1,36 @@
 const FormData = require("form-data");
 const path = require("path");
+const fs = require("fs");
 let url = "https://nekoweb.org/api/files/upload";
 const form = new FormData();
 let { APIKEY, DOMAIN, USERNAME, DIRECTORY } = process.env;
 
 const srcDir = path.resolve("..", DIRECTORY);
 
+function getAllFiles(dir, files = []) {
+    for (const file of fs.readDirSync(dir)) {
+        const fullPath = path.join(dir, file);
+        const stat = fs.statSync(fullPath);
+
+        if (stat.isDirectory()) {
+            getAllFiles(fullPath, files);
+        } else {
+            files.push(fullPath);
+        }
+    }
+    return files;
+}
+
+const files = getAllFiles(srcDir);
+
 form.append("pathname", `/${DIRECTORY}`);
-form.append("files", "test.txt", { filepath: srcDir });
+
+for (const filePath of files) {
+    const relative = path.relative(srcDir, filePath);
+    form.append("files", fs.createReadStream(filePath), {
+        filename: relative
+    });
+}
 
 let options = {
     method: "POST",
