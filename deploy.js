0ac96faee32d05be02b95f7ca875bb22c7f69678
Pufikas <pufikas@proton.me>
2026-01-13 11:44:37 +0200

    remove dir and add clone instead

--- deploy.js
+++ deploy.js
@@ -1,44 +1,50 @@
 const JSZip = require("jszip");
-
 const fs = require("fs");
 const fsp = require("fs/promises");
 const path = require("path");
 
-const { APIKEY, DOMAIN, USERNAME, DIRECTORY } = process.env;
+const { APIKEY, DOMAIN, USERNAME } = process.env;
 const url = "https://nekoweb.org/api/files/upload";
+
+const IGNORE = new Set([
+	".git",
+	".github",
+	"node_modules",
+	"deploytonekoweb",
+	"deploy.zip"
+]);
 
 async function zipDirectory(inputDir, outputZipPath) {
 	const zip = new JSZip();
-	const rootDir = path.basename(DOMAIN);
 
-	async function addFiles(dir, zipDir) {
-	const entries = await fsp.readdir(dir, { withFileTypes: true });
+	async function addFiles(dir, zipDir = "") {
+		const entries = await fsp.readdir(dir, { withFileTypes: true });
+		
+		for (const entry of entries) {
+				if (IGNORE.has(entry.name)) continue;
 
-	for (const entry of entries) {
-			const full = path.join(dir, entry.name);
-			const zipPath = path.join(zipDir, entry.name);
+				const full = path.join(dir, entry.name);
+				const zipPath = path.join(zipDir, entry.name);
 
-			if (entry.isDirectory()) {
-				await addFiles(full, zipPath);
-			} else {
-				zip.file(zipPath, await fsp.readFile(full));
+				if (entry.isDirectory()) {
+					await addFiles(full, zipPath);
+				} else {
+					zip.file(zipPath, await fsp.readFile(full));
+				}
 			}
 		}
-	}
-
-		await addFiles(inputDir, rootDir);
+		
+		await addFiles(inputDir);
 		const buf = await zip.generateAsync({ type: "nodebuffer" });
 		await fsp.writeFile(outputZipPath, buf);
 	}
 
 (async () => {
-	const inputDir = path.join(process.env.GITHUB_WORKSPACE, DIRECTORY);
-	const zipPath = path.join(__dirname, "deploy.zip");
+	const inputDir = path.join(process.cwd(), "site");
+	const zipPath = path.join(process.cwd(), "deploy.zip");
 
 	console.log("inputDir: ", inputDir);
 	console.log("zipPath: ", zipPath);
-
-	console.log("github_workspace:", process.env.GITHUB_WORKSPACE);
 	console.log("cwd:", process.cwd());
 
 
@@ -46,7 +52,6 @@ async function zipDirectory(inputDir, outputZipPath) {
 
 
 	const form = new FormData();
-
 	form.append("pathname", "/");
 
 	form.append(
@@ -64,7 +69,7 @@ async function zipDirectory(inputDir, outputZipPath) {
 	});
 	
 	console.log("zip size: ", (fs.statSync(zipPath).size / 1024 / 1024).toFixed(2), " MB");
-	console.log("res: ", res.status);
+	console.log("status: ", res.status);
 	console.log(await res.text());
 
 })();
